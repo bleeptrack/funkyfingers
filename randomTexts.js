bec4b6ec4a7ee4d208a1afa759964739e23e0055
zerbian <zerbian@campus.tu-berlin.de>
2021-05-30 22:34:34 +0200

    format tabs -> spaces

--- randomTexts.js
+++ randomTexts.js
@@ -1,10 +1,10 @@
 var randomText = [
     'WTF',
-	'OMG WTF',
+    'OMG WTF',
     'FUCK NFTs',
     'CORONA SUCKS',
     'FUCK UPLOAD FILTERS',
-	'FUCK 2020',
-	'WHY ?',
-	'THIS SUCKS'
+    'FUCK 2020',
+    'WHY ?',
+    'THIS SUCKS'
 ];
