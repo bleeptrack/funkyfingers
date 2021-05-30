113d08cae4918b4b6ba51ad8e9cf87a69fb0929a
zerbian <zerbian@campus.tu-berlin.de>
2021-05-30 22:33:01 +0200

    more random text fragments

--- randomTexts.js
+++ randomTexts.js
@@ -1,6 +1,10 @@
 var randomText = [
     'WTF',
+	'OMG WTF',
     'FUCK NFTs',
     'CORONA SUCKS',
-    'FUCK UPLOAD FILTERS'
+    'FUCK UPLOAD FILTERS',
+	'FUCK 2020',
+	'WHY ?',
+	'THIS SUCKS'
 ];
